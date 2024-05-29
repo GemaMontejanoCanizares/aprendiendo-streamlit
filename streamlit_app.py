804d58bf27635ce45121046698961db4c546923e
GemaMontejanoCanizares <163261827+GemaMontejanoCanizares@users.noreply.github.com>
2024-05-29 16:42:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Cargar datos
 @st.cache  # Esta línea ayuda a cachear los datos para que la app sea más rápida
 def load_data():
-    data = pd.read_csv('Peliculas.csv')
+    data = pd.read_csv('ruta_del_archivo.csv')
     return data
 
 data = load_data()
@@ -13,7 +14,16 @@ data = load_data()
 st.title('Gráfica de Películas por Año')
 
 # Agrupar por año y contar películas
-movies_per_year = data['Year'].value_counts().sort_index()
+movies_per_year = data.groupby('Year').size()
 
-# Crear gráfica usando Streamlit
-st.bar_chart(movies_per_year)
+# Crear gráfica
+fig, ax = plt.subplots()
+movies_per_year.plot(kind='bar', color='skyblue', ax=ax)
+plt.xlabel('Año')
+plt.ylabel('Número de Películas')
+plt.title('Número de Películas por Año')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Mostrar gráfica en Streamlit
+st.pyplot(fig)
