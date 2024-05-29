f6633592d31e1496f3aaf6393938c87720dde925
GemaMontejanoCanizares <163261827+GemaMontejanoCanizares@users.noreply.github.com>
2024-05-29 16:38:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Cargar datos
 @st.cache  # Esta línea ayuda a cachear los datos para que la app sea más rápida
 def load_data():
-    data = pd.read_csv('Peliculas.csv')
+    data = pd.read_csv('./devcontainer/Peliculas.csv')
     return data
 
 data = load_data()
